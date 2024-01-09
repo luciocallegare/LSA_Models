977bf0a507f46e4681e3b7f2de0bdf023d6163b7
Lucio Callegare <luciocallegare@gmail.com>
2024-01-09 02:30:04 -0300

    dibujar traduccion

--- mainLandmarks.py
+++ mainLandmarks.py
@@ -13,9 +13,20 @@ containers = mp.tasks.components.containers
 MEASURES_MODEL = 224
 N_FRAMES = 20
 
+
+def identifyHand(landmark,width,height):
+    maxHeight = int((max(landmark, key = lambda p: p.y).y)*height)+20
+    minHeight = int((min(landmark, key = lambda p:p.y).y)*height)-20
+    left =int((min(landmark, key = lambda p:p.x).x) *width)-20
+    right = int((max(landmark, key = lambda p:p.x).x)*width)+20
+    return {
+        'pt1':(left,maxHeight),
+        'pt2':(right,minHeight)
+    }
+
 def createHandsCanva(height,width,results):
     black_background = np.zeros((height,width,3),dtype = np.uint8)
-    for i,hand_landmarks in enumerate(results.multi_hand_landmarks):
+    for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(black_background,
                                     hand_landmarks,
                                     mp_hands.HAND_CONNECTIONS,
@@ -23,6 +34,19 @@ def createHandsCanva(height,width,results):
                                     #mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=10, circle_radius=2)
                                     )
     return black_background
+
+def drawRectangles(frame,results,width,height,lastSign):
+    if results.multi_hand_landmarks is None:
+        return frame
+    for hand_landmarks in results.multi_hand_landmarks:
+        handCoord = identifyHand(hand_landmarks.landmark,width,height)
+        color = (0,0,255)
+        frame = cv2.rectangle(frame,handCoord['pt1'],handCoord['pt2'],color,2)
+        if lastSign:
+            coordX = int(handCoord['pt1'][0]) + 10
+            coordY = handCoord['pt1'][1] + 15
+            frame = cv2.putText(frame,f"{lastSign['name']}:{lastSign['percentage']}%",(coordX,coordY),cv2.FONT_HERSHEY_SIMPLEX,0.5,color) 
+    return frame
 
 def main(path = 0, out = 'pruebas/prueba1.avi'):
     cap = cv2.VideoCapture(path)
@@ -33,7 +57,7 @@ def main(path = 0, out = 'pruebas/prueba1.avi'):
     #print(f'Path del video:{path}\nMedidas del video:{width}x{height}')
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(out, fourcc, 30.0, (WIDTH_CANVA,HEIGHT_CANVA), 1)
-    f = open('./dataset.json')
+    f = open('./dataset.json',encoding='utf-8')
     label = json.load(f)
     threshold = 0.5
     model = load_model('./models/modelConvLSTM', compile=False)
@@ -45,6 +69,7 @@ def main(path = 0, out = 'pruebas/prueba1.avi'):
             min_tracking_confidence = 0.5
         ) as hands:
         sequence = []
+        lastSign = {}
         time_stamp_before = time.time()
         i = 0
         while cap.isOpened():
@@ -55,15 +80,16 @@ def main(path = 0, out = 'pruebas/prueba1.avi'):
             fps = 1/(time_stamp_after-time_stamp_before)
             video_frames_count = fps*2
             skip_frames_window = int(video_frames_count/N_FRAMES)
-            frame = cv2.flip(frame,1)
-            cv2.imshow("Image", frame)
+            frame= cv2.flip(frame,1)
+            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(image_rgb)
+            frame = drawRectangles(frame,results,WIDTH_CANVA,HEIGHT_CANVA,lastSign)
             if i < skip_frames_window:
+                cv2.imshow("Image", frame)
                 i+=1
                 continue
             time_stamp_before = time_stamp_after
             height,width,_ = frame.shape
-            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = hands.process(image_rgb)
             if results.multi_hand_landmarks is not None:
                 handsCanva = createHandsCanva(HEIGHT_CANVA,WIDTH_CANVA,results)
                 handsCanva = cv2.cvtColor(handsCanva, cv2.COLOR_BGR2GRAY)
@@ -74,6 +100,7 @@ def main(path = 0, out = 'pruebas/prueba1.avi'):
             else:
                 i = 0
                 sequence = []
+                lastSign = {}
             if len(sequence) == N_FRAMES:
                 cantHands = len(results.multi_hand_landmarks)
                 probArray =  model.predict(np.expand_dims(sequence,0))
@@ -82,10 +109,17 @@ def main(path = 0, out = 'pruebas/prueba1.avi'):
                 cantHandsPred = 1 if label[idGesture]['handsUsed'] != 'B' else 2
                 print(idGesture,probPred)
                 if probPred > threshold and cantHands == cantHandsPred:
+                    lastSign = {
+                        "name":label[idGesture]["name"],
+                        "percentage":round(probPred*100,2)
+                    }
                     print(label[idGesture]["name"])
+                else:
+                    lastSign = {}
                 sequence = []
             if cv2.waitKey(1) & 0xFF == 27:
                 break
+            cv2.imshow("Image", frame)
             i = 0
 
     cap.release()
